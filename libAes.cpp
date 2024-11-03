a30d3803d094230a0485b82fd400b7e584ca49c1
Daniel Henrique Vieira <daniel.paladino@exati.com.br>
2024-11-03 18:11:36 -0300

    ADD: função shiftRow implementada e leitura dos parâmetros enviados por arquivo. sBox e rcon forão alterados para o aes.cpp, pois não estava sendo possível compilar com as variáveis declaradas no libAes

--- libAes.cpp
+++ libAes.cpp
@@ -1,6 +1,21 @@
 #include "libAes.hpp"
 
+void shiftRows(string * message) {
+    string shiftedMessage;
+
+    for (int row = 0; row < ROW_SIZE; row++) {
+        for (int column = 0; column < ROW_SIZE; column++) {
+            shiftedMessage.push_back(message->at(
+                (row * ROW_SIZE) + ((column + row) % ROW_SIZE)
+            ));
+        }   
+    }
+
+    *message = shiftedMessage;
+}
+
 string cryptograph(string message, string key) {
+    /*
     //addRoundKey(message, key);
     string roundKey;
     for(int round = 0; round < ROUNDS_QUANTITY; round++) {
@@ -11,6 +26,9 @@ string cryptograph(string message, string key) {
     }
 
     return " ";
+    */
+   shiftRows(&message);
+   return message;
 }
 
 string decryptograph(string message, string key) {
